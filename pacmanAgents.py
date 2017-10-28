7d6394f28626c0bffd58eecbbe336cdf3d097ccf
d3rpd3rp <johnled@notemachine.local>
2017-10-28 16:01:34 -0400

    farther along with Hill Climber

--- pacmanAgents.py
+++ pacmanAgents.py
@@ -42,7 +42,6 @@ class Node:
         else:
             return (sequence)
 
-
 class RandomAgent(Agent):
     # Initialization Function: Called one time when the game starts
     def registerInitialState(self, state):
@@ -98,65 +97,100 @@ class GreedyAgent(Agent):
         # return random action from the list of the best actions
         return random.choice(bestActions)
 
-class HillClimberAgent(Agent):
-    # Initialization Function: Called one time when the game starts
-    def registerInitialState(self, state):
-        return
-    
-    def searchNeighbors(neighbor):
-        bestScoreNode = neighbor
-        legal = neighbor.state.getAllPossibleActions()
-        for action in legal:
-            secondNeighborNode = Node(state.generatePacmanSuccessor(action), neighbor, action)
-            if secondNeighborNode.score > bestScoreNode.score:
-                bestScoreNode = secondNeighborNode
-            #already took two actions to get second neighbor, so only need a depth of 3 to return a max seq length of 4
-            secondNeighborBestNode = depthLimitedDFS(neighborNode, 0, 3)
-            if secondNeighborSearchSequenceBestNode.score > bestScoreNode.score:
-                bestScoreNode = secondNeighborSearchSequenceBestNode
-            
-    def depthLimitedDFS (neighbor, depth, limit, bestScoreNode):
-        legal = neighbor.state.getAllPossibleActions()
-        for action in legal:
-            nextNode = Node(state.generatePacmanSuccessor(action), neighbor, action)
+def searchNeighbors(neighbor):
+    bestScoreNode = neighbor
+    legal = neighbor.state.getAllPossibleActions()
+    for action in legal:
+        #print('neighbor has all the possible actions: {}'.format(legal))
+        global successorCallCount 
+        successorCallCount += 1
+        secondNeighborNode = Node(neighbor.state.generatePacmanSuccessor(action), neighbor, action)
+        if secondNeighborNode.score > bestScoreNode.score:
+            print('in seachNeighbors, assigning best score as {}'.format(bestScoreNode.score))
+            bestScoreNode = secondNeighborNode
+        #already took two actions to get second neighbor, so only need a depth of 3 to return a max seq length of 4
+        secondNeighborSearchBestNode = depthLimitedDFS(secondNeighborNode, 2, 5, bestScoreNode)
+        if secondNeighborSearchBestNode.score > bestScoreNode.score:
+            bestScoreNode = secondNeighborSearchBestNode
+    print ('search Neighbors returns Node is now of type {} and has score {}'.format(type(bestScoreNode), bestScoreNode.score))
+    return (bestScoreNode, neighbor.prevAction)
+
+
+def depthLimitedDFS (nextNeighbor, depth, limit, bestScoreNode):
+    #print('in limited DFS...bestScoreNode is of type {}'.format(type(bestScoreNode)))
+    legal = nextNeighbor.state.getAllPossibleActions()
+    for action in legal:
+        global successorCallCount 
+        successorCallCount += 1
+        nextState = nextNeighbor.state.generatePacmanSuccessor(action)
+        if nextState is not None: 
+            nextNode = Node(nextState, nextNeighbor, action)
             if nextNode.state.isLose():
                 if nextNode.score > bestScoreNode.score:
+                    print('in dlDFS - lost state, assigning best score as {}'.format(bestScoreNode.score))
                     bestScoreNode = nextNode
                 continue
             elif nextNode.state.isWin():
                 if nextNode.score > bestScoreNode.score:
+                    print('in dlDFS - win state, assigning best score as {}'.format(bestScoreNode.score))
                     bestScoreNode = nextNode 
                 continue
             else:
                 if depth <= limit:
                     if nextNode.score > bestScoreNode.score:
+                        print('in dlDFS - win state, assigning best score as {}'.format(bestScoreNode.score))
                         bestScoreNode = nextNode
+                        #print('in limited DFS...searching from node at depth {} with score {}'.format(depth, nextNode.score))
                     depth += 1
-                    DFS(nextNode, depth, limit)
+                    depthLimitedDFS(nextNode, depth, limit, bestScoreNode)
                 else:
                     if nextNode.score > bestScoreNode.score:
+                        print('in dlDFS - win state, assigning best score as {}'.format(bestScoreNode.score))
                         bestScoreNode = nextNode
-                    print('returning node with score {} as bestScoreNode from DFS.'.format(bestScoreNode.score))
-                    return (bestScoreNode)
+        else:
+            continue
+    #print('returning node with score {} as bestScoreNode from DFS.'.format(bestScoreNode.score))
+    return (bestScoreNode)
 
+class HillClimberAgent(Agent):
+    #some global counters
+    global successorCallCount, knownStates
+    successorCallCount = 0
+    knownStates = { }
+
+
+    # Initialization Function: Called one time when the game starts
+    def registerInitialState(self, state):
+        return
 
     # GetAction Function: Called with every frame
     def getAction(self, state):
+        #add first entry to dict of known states, for future optimization of not recomputing known state spaces
+        knownStates[state] = scoreEvaluation(state)
         #consider the request the root of search and create root (start) node
         startNode = Node(state)
         #initialize the bestNeighborNodes list (small dictionary of score, stop node)
         bestNeighborNodes = { }
         legal = state.getAllPossibleActions()
         for action in legal:
-            neighborNode = Node(state.generatePacmanSuccessor(action), startNode, action)
-            #not sure if test is going to break something...
-            if neighborNode.state.isLose():
-                #do I need to consider if this node has a high score on it's path somewhere?
-                continue
-            else:
-                searchNeighbors(neighborNode)
+            global successorCallCount 
+            successorCallCount += 1
+            nextState = state.generatePacmanSuccessor(action)
+            if nextState:
+                neighborNode = Node(state.generatePacmanSuccessor(action), startNode, action)
+                #test if this state is identical to a previous state from actions loop line 162
+                if knownStates[state]:
+                    continue
+                #not sure if test is going to break something...
+                elif neighborNode.state.isLose():
+                    #do I need to consider if this node has a high score on it's path somewhere?
+                    continue
+                else:
+                    bestScoreNode, firstAction = searchNeighbors(neighborNode)
+                    print('sequence from bestScoreNode traceback is {}'.format(bestScoreNode.traceback()))
+                    print('back in getAction for Hill Climber first action is {}.'.format(firstAction))
 
-        return Directions.STOP
+        return firstAction
 
 class GeneticAgent(Agent):
     # Initialization Function: Called one time when the game starts
